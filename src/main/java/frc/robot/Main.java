7e50f6fa3175375dc19ea3819df22c7bc7d461cd
Cady <candyzhouziqing@hotmail.com>
2020-01-25 14:33:16 -0500

    test

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
+package frc.robot; 
 
 import edu.wpi.first.wpilibj.RobotBase;
 
